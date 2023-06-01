75d66e3aac2994221e2e38fcc9bcfd5f9b31cca4
olachinkei <135185730+olachinkei@users.noreply.github.com>
2023-06-01 10:24:05 +0900

    Add files via upload

---
+++ ci.py
@@ -0,0 +1,3 @@
+import wandb
+print(f'The version of wandb is: {wandb.__version__}')
+assert wandb.__version__ == '0.15.2', f'Expected version 0.15.2, but got {wandb.__version__}'
---
+++ test-example/params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = "mlops-course-001"
+ENTITY = 'keisukekamata'
+BDD_CLASSES = {i:c for i,c in enumerate(['background', 'road', 'traffic light', 'traffic sign', 'person', 'vehicle', 'bicycle'])}
+RAW_DATA_AT = 'keisukekamata/mlops-course-001/bdd_simple_1k:v0'
+PROCESSED_DATA_AT = 'keisukekamata/mlops-course-001/bdd_simple_1k_split:v0'
